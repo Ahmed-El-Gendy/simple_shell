3f4c6a7a53df2657f59554c5f1655f1551532569
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-09-07 06:42:44 -0700

    push

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+
+#endif
