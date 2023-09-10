91d8cdb94dc200b9ef5fb2e809f215e2e17c8bee
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-09-10 11:59:51 -0700

    add

---
+++ handle_echo.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * handle_echo - handle echo case
+ * @input: string
+ * @args: arguments
+ * Return: void
+ */
+
+void handle_echo(char *input, char ***args)
+{
+	int arg_count;
+
+	*args = (char **)malloc(sizeof(char *));
+
+}
--- split.c
+++ split.c
@@ -9,15 +9,37 @@
  */
 void split(char *input, char **command, char ***args)
 {
-	int arg_count;
-	char *token = strtok(input, " ");
+	int arg_count, i, co = 0, j;
+	char *token = NULL;
+
+	for (i = 0; input[i] == ' '; i++);
+	for (co = i; input[co] != ' ' && input[co] != '\0'; co++);
+	token = malloc(sizeof(char) * co);
+	for (j = 0; i < co; i++, j++)
+	{
+		token[j] = input[i];
+	}
 
 	*command = token;
+	if (cmp(input, "echo"))
+	{
+		handle_echo(input, args);
+		return;
+	}
 	*args = (char **)malloc(sizeof(char *) * 256);
 	(*args)[0] = *command;
 	arg_count = 1;
-	while ((token = strtok(NULL, " ")) != NULL)
+	while (input[co] != '\0')
 	{
+		while (input[co] == ' ' && input[co] != '\0')
+			co++;
+		if (input[co] == '\0')
+				break;
+		for (i = 0; input[i + co] != ' ' && input[i + co] != '\0'; i++);
+		token = NULL;
+		token = malloc(sizeof(char) * i);
+		for (j = 0; input[co] != ' ' && input[co] != '\0'; co++, j++)
+			token[j] = input[co];
 		(*args)[arg_count] = token;
 		arg_count++;
 	}
